fe5f6f58b1cfd7bb77f3b743260a8574517dd5db
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-09 16:36:47 +0900

    Generic.cs

---
+++ Csharp/Generic.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        class Stack<T>
+        {
+            private T[] elements;
+            private int top;
+
+            public Stack()
+            {
+                elements = new T[100];
+                top = 0;
+            }
+
+            public void Push(T item)
+            {
+                elements[top++] = item;
+            }
+
+            public T Pop()
+            {
+                return elements[--top];
+            }
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+            Console.WriteLine(intStack.Pop());
+        }
+    }
+}
