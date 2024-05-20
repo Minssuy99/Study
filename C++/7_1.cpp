c0b5dbf6a6bb9743303996cd972cad9268df5e5c
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-21 01:47:57 +0900

    매개변수와 실인자

---
+++ C++/7_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int foo(int x, int y) // int x와 y 는 Parameter(매개변수) 라고 함.
+{
+    return x + y;
+}
+
+int main()
+{
+    int x = foo(1, 2); // 1과 2는 arguments. 인수, 인자라고 부른다.
+
+    cout << x << endl;
+
+    return 0;
+}
