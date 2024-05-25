54135344b100517a14ef33aa2a0b127d5d013fbc
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-26 02:13:04 +0900

    정적 다차원 배열

---
+++ C++/6_5.cpp
@@ -0,0 +1,36 @@
+/* 정적 다차원 배열 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int num_rows = 3;
+    const int num_columns = 5;
+
+
+
+    cout << endl;
+
+    int array[num_rows][num_columns] =  // row-major <-> column-major
+    {
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5}
+    };
+
+    for (int row = 0; row < num_rows; ++row)
+    {
+            for (int col = 0; col < num_columns; ++col)
+            {
+            // cout << '[' << row << ']' << '[' << col << ']' << '\t';
+            cout << (size_t)&array[row][col] << '\t';
+            cout << array[row][col] << '\t';
+            }
+            cout << endl;
+    }
+
+    return 0;
+}
+
